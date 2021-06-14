3e931737694e2cb36fcbaeb1540a2cace9b02e7d
AakashPawanGPS <AakashPawanGPS@github.com>
2021-06-14 20:19:37 +0530

    commit

--- Recursion/fibonacci_recur.cpp
+++ Recursion/fibonacci_recur.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int fib(int n){
+int fib(int n){                      //Tail Recursive
     int a=0;
     int b=1;
     int c=1;
@@ -16,9 +16,19 @@ int fib(int n){
     return b; 
 }
 
+int fibo(int n){                     //Recursive
+    if(n==0){
+        return 0; 
+    }
+    if(n==1){
+        return 1;
+    }
+    return fibo(n-1)+fibo(n-2);
+}
+
 int main(){
     int n=6;
-    int k=fib(n);
+    int k=fibo(n);
     cout<<k;
     return 0;
 }
