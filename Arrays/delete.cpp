3750c1735eb25df72dde462467e2309be270eb51
AakashPawanGPS <AakashPawanGPS@github.com>
2021-06-16 16:12:05 +0530

    commit

--- Arrays/delete.cpp
+++ Arrays/delete.cpp
@@ -8,6 +8,9 @@ int delet(int arr[] ,int n,  int x){
             break;
         }
     }
+    if(i==n){
+        return -1;
+    }
     for(int j=i;j<n-1;j++){
         arr[j]=arr[j+1];
     }
@@ -18,7 +21,7 @@ int delet(int arr[] ,int n,  int x){
 int main(){
     int arr[]={3,5,7,9,12};
     int n=sizeof(arr)/sizeof(arr[0]);
-    int x=7;
+    int x=8;
     int k=delet(arr,n,x);
     cout<<k;
     return 0;
