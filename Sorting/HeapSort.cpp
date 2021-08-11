c95cbda982628be7a5003044f662d05929825d7f
Aakash Pawan G P S <aakashpawangps@gmail.com>
2021-08-11 23:51:54 +0530

    Heap Sort Algo

---
+++ Sorting/HeapSort.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void maxHeapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i)
+    {
+        swap(arr[largest], arr[i]);
+        maxHeapify(arr, n, largest);
+    }
+}
+
+void buildHeap(int arr[], int n)
+{
+    for (int i = (n - 2) / 2; i >= 0; i--)
+    {
+        maxHeapify(arr, n, i);
+    }
+}
+
+void HeapSort(int arr[], int n)
+{
+    buildHeap(arr, n);
+    for (int i = n - 1; i >= 1; i--)
+    {
+        swap(arr[0], arr[i]);
+        maxHeapify(arr, i, 0);
+    }
+}
+
+int main()
+{
+    int arr[] = {7, 1, 5, 2, 9, 6, 8};
+    int n = 7;
+
+    HeapSort(arr, n);
+    for (int i : arr)
+        cout << i << " ";
+    return 0;
+}
