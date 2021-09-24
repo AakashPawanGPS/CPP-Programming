94468d24f6df7a9ca40f59e6c9c752536cef27a5
Aakash Pawan G P S <aakashpawangps@gmail.com>
2021-09-24 14:08:38 +0530

    Effective Approach

--- Searching Algo/EffectiveApproach.cpp
+++ Searching Algo/EffectiveApproach.cpp
@@ -3,25 +3,25 @@ using namespace std;
 
 int main()
 {
-    int n;
+    long long int n;
     cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
+    long long int arr[n];
+    for (long long int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
-    int q;
+    long long int q;
     cin >> q;
-    int query[q];
-    for (int i = 0; i < q; i++)
+    long long int query[q];
+    for (long long int i = 0; i < q; i++)
     {
         cin >> query[i];
     }
-    int v = 0, p = 0;
-    for (int i = 0; i < q; i++)
+    long long int v = 0, p = 0;
+    for (long long int i = 0; i < q; i++)
     {
-        int x = query[i];
-        for (int j = 0; j < n; j++)
+        long long int x = query[i];
+        for (long long int j = 0; j < n; j++)
         {
             if (x == arr[j])
             {
@@ -29,12 +29,12 @@ int main()
                 break;
             }
         }
-        for (int j = n - 1; j >= 0; j--)
+        for (long long int j = n - 1; j >= 0; j--)
         {
             if (query[i] == arr[j])
                 p += (n - j);
         }
     }
-    cout << v << " " << p;
+    printf("%I64d %I64d\n", v, p);
     return 0;
 }
